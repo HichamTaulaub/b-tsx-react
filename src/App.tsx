6e60342b6e42d37ea328b719d0eec70a24ded551
ABADOU Chaouki <abadouchaouki33@gmail.com>
2025-03-29 10:58:14 +0000

    first commit of Building React Apps with TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles.css';
+import Greeting from './components/Greeting';
+import Counter from './Counter';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Welcome to My React App</h1>
+      <Greeting name="Chaouki" />
+      <Counter />
     </div>
   );
 }
 
 export default App;
+
